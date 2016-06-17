4a136ae449a8eab192188845b2ba60fcd190f364
Danny Willems <contact@danny-willems.be>
2016-06-17 20:17:21 +0200

    Use functions instead of objects

--- cordova_progress.mli
+++ cordova_progress.mli
@@ -1,48 +1,36 @@
 (* -------------------------------------------------------------------------- *)
 (* See the official documentation *)
-class progress : Ojs.t ->
-  object
-    inherit Ojs.obj
-    (* ---------------------------------------------------------------------- *)
-    (* show a progress loading *)
-    (* show ?[string_to_show] *)
-    method show     : ?text:(string [@js.default "Loading..."])   ->
-                      unit                                        ->
-                      unit
-    (* show_cb ?[success_callback] ?[error_callback] ?[string_to_show] *)
-    method show_cb  :  ?succ_cb:((unit -> unit) [@js.default (fun () -> ())]) ->
-                       ?err_cb:((unit -> unit) [@js.default (fun () -> ())])  ->
-                       ?text:(string [@js.default "Loading..."])              ->
-                       unit                                                   ->
-                       unit
-    (* ---------------------------------------------------------------------- *)
+(* ---------------------------------------------------------------------- *)
+(* show a progress loading *)
+(* show_cb ?[success_callback] ?[error_callback] ?[string_to_show] *)
+val show_cb :
+  ?succ_cb:((unit -> unit) [@js.default (fun () -> ())]) ->
+  ?err_cb:((unit -> unit) [@js.default (fun () -> ())])  ->
+  ?text:(string [@js.default "Loading..."])              ->
+  unit                                                   ->
+  unit
+[@@js.global "progress.show"]
+(* ---------------------------------------------------------------------- *)
 
-    (* ---------------------------------------------------------------------- *)
-    (* Update the progress loading *)
-    (* update *)
-    method update    : ?text:(string [@js.default "Loading..."])   ->
-                       unit                                        ->
-                       unit
-    (* update_cb ?[success_callback] ?[error_callback] ?[string_to_show] *)
-    method update_cb : ?succ_cb:((unit -> unit) [@js.default (fun () -> ())]) ->
-                       ?err_cb:((unit -> unit) [@js.default (fun () -> ())])  ->
-                       ?text:(string [@js.default "Loading..."])              ->
-                       unit                                                   ->
-                       unit
-    (* ---------------------------------------------------------------------- *)
+(* ---------------------------------------------------------------------- *)
+(* update_cb ?[success_callback] ?[error_callback] ?[string_to_show] *)
+val update :
+  ?succ_cb:((unit -> unit) [@js.default (fun () -> ())]) ->
+  ?err_cb:((unit -> unit) [@js.default (fun () -> ())])  ->
+  ?text:(string [@js.default "Loading..."])              ->
+  unit                                                   ->
+  unit
+[@@js.global "progress.update"]
+(* ---------------------------------------------------------------------- *)
 
-    (* ---------------------------------------------------------------------- *)
-    (* hide the progress loading *)
-    (* hide ?[success_callback] ?[error_callback] *)
-    method hide : ?succ_cb:((unit -> unit) [@js.default (fun () -> ())])  ->
-                  ?err_cb:((unit -> unit) [@js.default (fun () -> ())])   ->
-                  unit                                                    ->
-                  unit
-    (* ---------------------------------------------------------------------- *)
-  end
-(* -------------------------------------------------------------------------- *)
-
-(* -------------------------------------------------------------------------- *)
-val t : unit -> progress
-[@@js.get "progress"]
+(* ---------------------------------------------------------------------- *)
+(* hide the progress loading *)
+(* hide ?[success_callback] ?[error_callback] *)
+val hide :
+  ?succ_cb:((unit -> unit) [@js.default (fun () -> ())])  ->
+  ?err_cb:((unit -> unit) [@js.default (fun () -> ())])   ->
+  unit                                                    ->
+  unit
+[@@js.global "progress.hide"]
+(* ---------------------------------------------------------------------- *)
 (* -------------------------------------------------------------------------- *)
